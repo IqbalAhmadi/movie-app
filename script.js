ffafefcab058113a4987939dd70cc4c88e655ea9
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-05-03 12:01:27 -0700

    fetch the api key from themoviedb.org, created search varibale

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//TODO: Fetching data from API from (themoviedb.org)
 
+const API_URL =
+  'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=624800e6ad063d96e7db6c2e0c5b4eb1&page=1'
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+// search endpoint
+const SEARCH_URL =
+  'http://api.themoviedb.org/3/search/movie?api_key=624800e6ad063d96e7db6c2e0c5b4eb1&query="'
