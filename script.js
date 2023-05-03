0e9e7bf8c3282fb35dcdfa7ae23a37d1a3c59c42
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-05-03 12:14:47 -0700

    created eventlisteners and tested to see if I get movies search result in the terminal

--- script.js
+++ script.js
@@ -6,5 +6,31 @@ const API_URL =
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 
 // search endpoint
-const SEARCH_URL =
+const SEARCH_API =
   'http://api.themoviedb.org/3/search/movie?api_key=624800e6ad063d96e7db6c2e0c5b4eb1&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+// Get initial movies
+getMovies(API_URL)
+
+async function getMovies(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(data.results)
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value
+  if (searchTerm && searchTerm !== '') {
+    getMovies(SEARCH_API + searchTerm)
+
+    search.value = ''
+  } else {
+    window.location.reload()
+  }
+})
