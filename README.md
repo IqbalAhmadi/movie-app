a13a5d5b4f802d837607eb133931f2f661d2b83e
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-05-04 11:47:37 -0700

    updated readme file with descriptions and a demo gif file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Under construction 🔥🛠️🎬
+
+# Movie Application
+
+This is a movie application built with HTML, CSS, and JavaScript that allows users to search for movies based on keywords and displays at least 20 results using a third-party API. When the user hovers over a movie poster, the movie's description is displayed.
+
+![Demo](./asset/movie-app.gif)
+
+## Getting Started
+
+To get started, clone this repository to your local machine or download the zip file and extract it.
+
+`git clone https://github.com/IqbalAhmadi/movie-app.git`
+
+## Prerequisites
+
+You will need a modern web browser and a code editor like Visual Studio Code, Atom, or Sublime Text.
+
+## API Usage
+
+This application uses the OMDb API (https://www.themoviedb.org/) to retrieve movie information. To use the API, you will need to obtain an API key from the website.
+
+Go to https://www.themoviedb.org/settings/api.
+Fill out the form and click "Submit".
+Your API key should now be displayed on the page.
+Once you have your API key, open the `script.js` file and replace `YOUR_API_KEY_HERE` with your actual API key.
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+- themoviedb API
--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API =
   'http://api.themoviedb.org/3/search/movie?api_key=624800e6ad063d96e7db6c2e0c5b4eb1&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -19,7 +20,44 @@ async function getMovies(url) {
   const res = await fetch(url)
   const data = await res.json()
 
-  console.log(data.results)
+  showMovies(data.results)
+}
+
+function showMovies(movies) {
+  main.innerHTML = '' // this clears the cards
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie
+
+    // creating movie element
+    const movieEl = document.createElement('div')
+    movieEl.classList.add('movie')
+    movieEl.innerHTML = `
+    
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+   
+    `
+    main.appendChild(movieEl)
+  })
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green'
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
 }
 
 form.addEventListener('submit', (e) => {
